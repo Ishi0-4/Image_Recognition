ecabf8d8ccaba18530769c099ba309430080fbda
Ishi0-4 <91232581+Ishi0-4@users.noreply.github.com>
2024-08-01 11:12:59 +0530

    first commit

---
+++ resize.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os
+
+def resize_images(input_folder, output_folder, target_size):
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Loop through all files in the input folder
+    for filename in os.listdir(input_folder):
+        try:
+            # Open the image file
+            with Image.open(os.path.join(input_folder, filename)) as img:
+                # Resize the image
+                img_resized = img.resize(target_size)
+                # Save the resized image to the output folder
+                img_resized.save(os.path.join(output_folder, filename))
+                print(f"Resized {filename}")
+        except Exception as e:
+            print(f"Error processing {filename}: {e}")
+
+# Example usage:
+
+input_folder = "C:\\Users\\lenovo\\OneDrive\\Images\\cropped\\train\\n02085620-Chihuahua"
+output_folder = "C:\\Users\\lenovo\\OneDrive\\I_R"
+target_size = (224,224)  # Specify the target size (width, height) in pixels
+resize_images(input_folder, output_folder, target_size)
